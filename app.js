8b1cca606b413b2c278809e0cc54ef634cfbfc90
Manaal Syed <msyed@wpi.edu>
2021-12-31 21:36:18 -0500

    update

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ import bodyParser from "body-parser"
 import express from "express"
 import ejs from "ejs"
 import mongoose from "mongoose"
-import encrypt from "mongoose-encryption"
 import dotenv from 'dotenv'
+import crypto from "crypto"
 dotenv.config()
+
+function sha256(data) {
+    return crypto.createHash("sha256").update(data, "binary").digest("base64");
+}
 
 //initialize app
 const app = express();
@@ -25,9 +29,6 @@ const userSchema = new mongoose.Schema({
     password: String
 })
 
-const secret = process.env.SECRET
-userSchema.plugin(encrypt, { secret: secret, encryptedField: ["password"] })
-
 const User = mongoose.model("User", userSchema)
 
 app.get("/", (req, res) => {
@@ -40,7 +41,7 @@ app.route("/login")
     })
     .post((req, res) => {
         const loginEmail = req.body.username
-        const password = req.body.password
+        const password = sha256(req.body.password)
         User.findOne({ email: loginEmail }, (err, user) => {
             if (!err) {
                 if (user) {
@@ -66,7 +67,7 @@ app.route("/register")
     .post((req, res) => {
         const newUser = new User({
             email: req.body.username,
-            password: req.body.password
+            password: sha256(req.body.password)
         })
         newUser.save((err) => {
             if (!err) {
